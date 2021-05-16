51c3a6e74067fac280923163e512fcd71714df2d
Avinash Tomar <avinash.tomar@zebra.com>
2021-05-16 08:45:15 -0400

    Styling for team page

--- src/frontend/src/components/MatchDetailsCard.js
+++ src/frontend/src/components/MatchDetailsCard.js
@@ -1,17 +1,31 @@
 import {React} from'react';
 import {Link} from 'react-router-dom';
+import "./MatchDetailsCard.css";
+
 export const MatchDetailsCard = ({teamName,match}) => {
  if(!match) return null;
  
  const otherTeam = match.team1===teamName?match.team2:match.team1;
  const otherTeamRoute = `/teams/${otherTeam}`;
+ const isMatchWon = teamName===match.winner;
+
   return (
-    <div className="MatchDetailsCard">
-     <h3>Latest Matches</h3>
-     <h1>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h1>
-     <h2>{match.date}</h2>
-     <h3>at {match.venue}</h3>
-     <h3>{match.winner} won by {match.resultMargin} {match.result}</h3>
+    <div className={isMatchWon ? 'MatchDetailsCard won-card':'MatchDetailsCard lost-card'}>
+      <div>
+        <span className="vs">vs</span>
+        <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
+        <h2 className="match-date">{match.date}</h2>
+        <h3 className="match-venue">at {match.venue}</h3>
+        <h3 className="match-result">{match.winner} won by {match.resultMargin} {match.result}</h3>
+      </div> 
+      <div className="additional-detail">
+          <h3>First Innings</h3>
+          <p>{match.team1}</p>
+          <h3>Second Innings</h3>
+          <p>{match.team2}</p>
+          <h3>Man of the match</h3>
+          <p>{match.playerOfMatch}</p>
+      </div>
     </div>
   );
 }
